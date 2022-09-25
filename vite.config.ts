cdd08c839099269ab1d19b17a0131da7e81a3feb
volte <100103+volte@users.noreply.github.com>
2022-09-25 10:41:30 -0400

    Small changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import {defineConfig} from "vite";
+import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import ViteYaml from "@modyfi/vite-plugin-yaml";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -6,4 +6,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), react(), ViteYaml()],
+  server: {
+    host: "0.0.0.0",
+  },
 });
